0136de079ffe40f808b0c5b678e25d594f59326c
Andrey <andrey2004182@gmail.com>
2022-10-25 15:41:05 +0300

    Complete

---
+++ AllLabs/Lab7/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace AllLabsProject
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        [DataMember]
+        public string Gender { get; set; }
+    }
+}
---
+++ AllLabs/Lab7/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace AllLabsProject
+{
+    [DataContract]
+    public class Student : Person
+    {
+        [DataMember]
+        public string Group { get; set; }
+        [DataMember]
+        public string Institution { get; set; }
+        [DataMember]
+        public string Specialization { get; set; }
+
+    }
+}
